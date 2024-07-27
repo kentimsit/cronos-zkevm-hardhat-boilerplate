6c0ce8a9d916885de17409bf9ff9eef104575243
Steven Nguyen <steven.nguyen@cronoslabs.org>
2024-07-27 10:33:37 +0700

    fix spacing

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,44 +12,44 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const config: HardhatUserConfig = {
-  defaultNetwork: "cronosZkEvmTestnet",
-  networks: {
-    hardhat: {
-      zksync: false,
+    defaultNetwork: "cronosZkEvmTestnet",
+    networks: {
+        hardhat: {
+            zksync: false,
+        },
+        zkSyncMainnet: {
+            url: "https://mainnet.era.zksync.io",
+            ethNetwork: "mainnet",
+            zksync: true,
+            verifyURL:
+                "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+        },
+        cronosZkEvmTestnet: {
+            url: "https://testnet.zkevm.cronos.org",
+            ethNetwork: "sepolia", // or a Sepolia RPC endpoint from Infura/Alchemy/Chainstack etc.
+            zksync: true,
+            verifyURL: "https://explorer.zkevm.cronos.org/contract_verification",
+        },
     },
-    zkSyncMainnet: {
-      url: "https://mainnet.era.zksync.io",
-      ethNetwork: "mainnet",
-      zksync: true,
-      verifyURL:
-        "https://zksync2-mainnet-explorer.zksync.io/contract_verification",
+    zksolc: {
+        // For Cronos zkEVM, currently only supports zksolc version up to 1.4.1 for contract verification
+        version: "1.4.1",
+        settings: {
+            // find all available options in the official documentation
+            // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-solc.html#configuration
+        },
     },
-    cronosZkEvmTestnet: {
-      url: "https://testnet.zkevm.cronos.org",
-      ethNetwork: "sepolia", // or a Sepolia RPC endpoint from Infura/Alchemy/Chainstack etc.
-      zksync: true,
-      verifyURL: "https://explorer.zkevm.cronos.org/contract_verification",
+    solidity: {
+        // For Cronos zkEVM, currently only supports solidity version up to 0.8.25 for contract verification
+        version: "0.8.24",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+            viaIR: true,
+        },
     },
-  },
-  zksolc: {
-    // For Cronos zkEVM, currently only supports zksolc version up to 1.4.1 for contract verification
-    version: "1.4.1",
-    settings: {
-      // find all available options in the official documentation
-      // https://era.zksync.io/docs/tools/hardhat/hardhat-zksync-solc.html#configuration
-    },
-  },
-  solidity: {
-    // For Cronos zkEVM, currently only supports solidity version up to 0.8.25 for contract verification
-    version: "0.8.24",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200,
-      },
-      viaIR: true,
-    },
-  },
 };
 
 export default config;
